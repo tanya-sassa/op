c36ecd411a3eed75821c65e4532ccf622952dcf8
tanya-sassa <144169146+tanya-sassa@users.noreply.github.com>
2023-11-20 18:29:58 +0300

    Delete задание 3(i).py

--- задание 3(i).py
+++
@@ -1,8 +0,0 @@
-age=int(input())
-if age >0 and age<75:
-    if age >= 16:
-        print('Поздравляем  вы поступили в ВГУИТ')
-    else:
-        print('Сначала нужно закончить школу!')
-else:
-    print('Ошибка')
