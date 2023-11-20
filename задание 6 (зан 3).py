2337b10b4c123705b2f59d0934bf87acc9b0e291
tanya-sassa <144169146+tanya-sassa@users.noreply.github.com>
2023-11-20 18:18:17 +0300

    Delete задание 6 (зан 3).py

--- задание 6 (зан 3).py
+++
@@ -1,8 +0,0 @@
-per=int(input())
-vtor=int(input())
-tret=int(input())
-chetv=int(input())
-if ((per%2==0 and vtor%2==0) and (tret%2!=0 and chetv%2!=0))or((per%2!=0 and vtor%2!=0) and (tret%2==0 and chetv%2==0)):
-    print('Да')
-else:
-    print('Нет')
